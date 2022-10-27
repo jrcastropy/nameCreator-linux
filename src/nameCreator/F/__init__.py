5e1f6b2fdbff3a5f2c4852370a1a826ae71e03bd
Jayr <jayrcastro.py@gmail.com>
2022-10-27 19:30:52 +0800

    removed src folder added gitignore

--- src/nameCreator/F/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-
-Generate a female first name
-
-"""
-
-__version__ = "0.8.4"
-__author__ = 'Justin Huddleston'
--- src/nameCreator/L/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Generate a last name
-
-"""
-
-__version__ = "0.8.4"
-__author__ = 'Justin Huddleston'
